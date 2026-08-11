c1c5bb3fece5c2734b54175c5b89a2de66094c88
Brian Rios <brian.rios977@gmail.com>
2026-08-11 14:15:04 -0300

    feat: inicializar proyecto Unidad_1_Fundamentos con Spring Boot

---
+++ Unidad_1_Fundamentos/src/test/java/com/utn/unidad_1_fundamentos/Unidad1FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.unidad_1_fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unidad1FundamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
